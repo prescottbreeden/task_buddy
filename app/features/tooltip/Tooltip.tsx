4c61cb3f6c4f81a988731b061e602f8fa295f847
prescottbreeden <prescott.breeden@gmail.com>
2020-07-05 13:02:49 -0700

    organized sass and added media queries

---
+++ app/features/tooltip/Tooltip.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type TooltipComponentProps = {
+  indicator: string | JSX.Element;
+  tooltip: string | JSX.Element;
+};
+
+export const Tooltip: React.FC<TooltipComponentProps> = (props) => {
+  const { indicator, tooltip } = props;
+
+  const tooltipClassName = () => {
+    if (typeof tooltip === 'string') {
+      return tooltip.length > 70
+        ? 'tooltiptext tooltiptext--long'
+        : 'tooltiptext tooltiptext--short';
+    }
+    return 'tooltiptext--long';
+  };
+  return (
+    <div className="tooltip" aria-hidden="true">
+      {indicator}
+      <span className={tooltipClassName()}>{tooltip}</span>
+    </div>
+  );
+};
+
